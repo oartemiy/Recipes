30485fa484a7fa9adfa945e9b0a1e6a80c0be885
oartemiy <artemiy.obraztsov@yandex.ru>
2025-11-23 18:58:21 +0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Артемий Образцов on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
